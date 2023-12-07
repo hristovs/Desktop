69c934f705b73f4df0691da346199b95f725b133
shristov <shristov@microsoft.com>
2023-12-07 01:19:53 +0000

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          The most stable made this.
+          Tell us about your landlord.
         </p>
       </header>
     </div>
